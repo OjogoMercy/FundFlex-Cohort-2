55158f4aa9a882c12e7157f9eb381e5c04df7ce9
Armykookie35 <mercyojogo@gmail.com>
2025-08-26 14:40:15 +0100

    ...

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import { StyleSheet, Text, View } from "react-native";
 import CustomButton from "./src/components/CustomButton";
 import CustomHeader from "./src/components/CustomHeader";
 import { Colors } from "./src/constants/Theme";
+import CustomInput from "./src/components/CustomInput";
+import { useState } from "react";
 export default function App() {
+  const [on, off] = useState('')
   return (
     <View style={styles.container}>
       <CustomHeader title="index" />
       <CustomButton title="buttonComponent" />
+      <CustomInput value={ on} onChangeText={off} />
     </View>
   );
 }
@@ -16,6 +20,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    
     alignItems: "center",
   },
 });
