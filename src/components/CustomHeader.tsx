36907be4e0b0a7881b057542c2d14c248baa711f
Armykookie35 <mercyojogo@gmail.com>
2025-08-26 14:06:50 +0100

    ...

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -2,9 +2,9 @@ import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
 import React from 'react'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { Colors,FONTS,SCREEN_HEIGHT,SCREEN_WIDTH } from '../constants/Theme';
-// import { useNavigation } from 'expo-router';
+import { useNavigation } from 'expo-router';
 const CustomHeader = ({ title }) => {
-  // const navigation = useNavigation();
+  const navigation = useNavigation();
   return (
     <View style={{ flexDirection: "row" ,}}>
       <TouchableOpacity onPress={() => navigation.goBack()}>
