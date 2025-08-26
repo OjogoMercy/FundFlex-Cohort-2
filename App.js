0418263fafc028447d76d6afa27f355e26066aa9
Armykookie35 <mercyojogo@gmail.com>
2025-08-26 14:37:17 +0100

    ...

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import CustomButton from "./src/components/CustomButton";
 import CustomHeader from "./src/components/CustomHeader";
+import { Colors } from "./src/constants/Theme";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <CustomHeader title="index" />
       <CustomButton title="buttonComponent" />
-      <StatusBar style="auto" />
     </View>
   );
 }
