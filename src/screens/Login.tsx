7a538fc43bdb4fc8be74c51f941206ece3097b00
Armykookie35 <mercyojogo@gmail.com>
2025-08-27 14:32:27 +0100

    ...

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import CustomInput from '../components/CustomInput'
 
 const Login = () => {
+    const [input, setInput] = useState('')
+
   return (
     <View>
-      <Text>Login</Text>
+          <Text>Login</Text>
+          <CustomInput value={input} onChangeText={setInput } />
     </View>
   )
 }
