5183f778aced0bf64cadb77eee89043c980b21f3
Armykookie35 <mercyojogo@gmail.com>
2025-09-02 17:16:13 +0100

    ...

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -1,48 +1,43 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import AntDesign from "@expo/vector-icons/AntDesign";
-import { Colors, FONTS, SCREEN_HEIGHT, SCREEN_WIDTH } from "../constants/Theme";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Colors, FONTS, Sizes } from "../constants/Theme";
 import { StatusBar } from "react-native";
 import { useNavigation } from 'expo-router';
 const CustomHeader = ({ title }) => {
   const navigation = useNavigation();
   return (
-      <View
+    <View
+      style={{
+        flexDirection: "row",
+        backgroundColor: Colors.primary,
+        height: Sizes.height* 0.1,
+        width: Sizes.width * 1,
+        alignItems: "center",
+        justifyContent: "flex-start",
+        gap:150,
+        padding:5
+      }}
+    >
+    <StatusBar backgroundColor="white" barStyle={'light-content'} />
+
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <AntDesign
+          name="arrowleft"
+          size={Sizes.height* 0.03}
+          color={Colors.white}
+        />
+      </TouchableOpacity> 
+      <Text
         style={{
-          flexDirection: "row",
-          backgroundColor: Colors.primary,
-          height: SCREEN_HEIGHT * 0.1,
-          width: SCREEN_WIDTH * 1,
-          alignItems: "center",
-          justifyContent: "center",
+          ...FONTS.h2,
+          textAlign: "center",
+          color: Colors.white,
         }}
       >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "flex-start",
-            gap: 100,
-            width: SCREEN_WIDTH * 0.9,
-          }}
-        >
-          <TouchableOpacity>
-            <AntDesign name="arrowleft" size={24} color="white" />
-            
-          </TouchableOpacity>
-          <Text
-            style={{
-              ...FONTS.h2,
-              textAlign: "center",
-              color: Colors.white,
-            }}
-          >
-            {title}
-          </Text>
-        </View>
-      </View>
-    
+        {title}
+      </Text>
+    </View>
   );
 };
 
--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -19,7 +19,7 @@ import ResetPassword1 from "../screens/ResetPassword1";
 import PrivacyPolicy from "../screens/PrivacyPolicy";
 import Facial from "../screens/Facial";
 import Personal from "../screens/Personal";
-import VerifyCode from '../screens/VerifyCode1';
+import VerifyCode from "../screens/VerifyCode1";
 import ResetPassword from "../screens/ResetPassword1";
 import ResetPassword2 from "../screens/Resetpassword2";
 import VerifyCode2 from "../screens/VerifyCode1";
@@ -28,39 +28,38 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
-    return (
-        
-      <NavigationIndependentTree>
-        <NavigationContainer>
-                <Stack.Navigator initialRouteName="Personal" screenOptions={{ headerShown: false }}>
-          <Stack.Navigator initialRouteName="Profile" screenOptions={{headerShown: false}}>
-                <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
-                    
-            <Stack.Screen
-              name="BottomTab"
-              component={BottomTab}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="Splash" component={Splash} />
-            <Stack.Screen name="IntroScreen1" component={IntroScreen1} />
-            <Stack.Screen name="IntroScreen2" component={IntroScreen2} />
-            <Stack.Screen name="ResetPin3" component={ResetPin3} />
-            <Stack.Screen name="About" component={About} />
-            <Stack.Screen name="ResetPassword1" component={ResetPassword1} />
-            <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicy} />
-            <Stack.Screen name="Facial" component={Facial} />
-            <Stack.Screen name="Personal" component={Personal} />
-            <Stack.Screen name="VerifyCode" component={VerifyCode} />
-            <Stack.Screen name="ResetPassword" component={ResetPassword} />
-            <Stack.Screen name="ResetPassword2" component={ResetPassword2} />
-            <Stack.Screen name="VerifyCode2" component={VerifyCode2} />
-            <Stack.Screen name="Profile" component={Profile} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </NavigationIndependentTree>
-    );
+  return (
+    <NavigationIndependentTree>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Splash"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen
+            name="BottomTab"
+            component={BottomTab}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Splash" component={Splash} />
+          <Stack.Screen name="IntroScreen1" component={IntroScreen1} />
+          <Stack.Screen name="IntroScreen2" component={IntroScreen2} />
+          <Stack.Screen name="ResetPin3" component={ResetPin3} />
+          <Stack.Screen name="About" component={About} />
+          <Stack.Screen name="ResetPassword1" component={ResetPassword1} />
+          <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicy} />
+          <Stack.Screen name="Facial" component={Facial} />
+          <Stack.Screen name="Personal" component={Personal} />
+          <Stack.Screen name="VerifyCode" component={VerifyCode} />
+          <Stack.Screen name="ResetPassword" component={ResetPassword} />
+          <Stack.Screen name="ResetPassword2" component={ResetPassword2} />
+          <Stack.Screen name="VerifyCode2" component={VerifyCode2} />
+          <Stack.Screen name="Profile" component={Profile} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NavigationIndependentTree>
+  );
 }
 export function BottomTab() {
   return (
